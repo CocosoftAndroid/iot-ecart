6b688a54c590998f7d7f100a271a857f8ab9392b
Muneeswaraiah Kasaram <Muneeswaraiah Kasaram>
2017-05-13 19:11:53 -0700

    IOT Ecart application changes

---
+++ CocoNFCTag/app/src/main/java/com/example/coco/coconfctag/retrofit/RetrofitAPIClient.java
@@ -0,0 +1,26 @@
+package com.example.coco.coconfctag.retrofit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by cocoadmin on 5/3/2017.
+ */
+
+public class RetrofitAPIClient {
+
+    private static Retrofit retrofit = null;
+    static Retrofit getClient() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://reqres.in")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+        return retrofit;
+    }
+}
